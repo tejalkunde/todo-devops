ec6f19057b4df1dee9d2758a4d31d480a94c0dfb
tejalkunde <kunde.tejal293@gmail.com>
2026-07-18 08:51:47 +0530

    Add backend Todo API with Docker configuration

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+const todoRoutes = require("./routes/todoRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Todo Backend Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
