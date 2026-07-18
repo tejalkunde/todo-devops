41d1fa59358761b40fd7b8555d1e8de5b83a54ee
tejalkunde <kunde.tejal293@gmail.com>
2026-07-18 07:36:16 +0530

    feat: add MongoDB and Docker Compose setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
+<<<<<<< HEAD
 const express = require("express");
 
+=======
+require("dotenv").config();
+
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+>>>>>>> e3e3eab (feat: add MongoDB and Docker Compose setup)
 const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
 
+<<<<<<< HEAD
+=======
+connectDB();
+
+>>>>>>> e3e3eab (feat: add MongoDB and Docker Compose setup)
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
@@ -12,8 +26,15 @@ app.get("/", (req, res) => {
     res.send("Todo Backend Running...");
 });
 
+<<<<<<< HEAD
 const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+=======
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+>>>>>>> e3e3eab (feat: add MongoDB and Docker Compose setup)
 });
